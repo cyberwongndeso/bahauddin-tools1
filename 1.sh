b1cf78e8f2e5890ff2276ba3c16bcd817fec086d
cyberwongndeso <42183115+cyberwongndeso@users.noreply.github.com>
2019-01-03 01:26:08 +0700

    Vvhvfdd
    Gfdsssff

---
+++ 1.sh
@@ -0,0 +1,48 @@
+#!/system/xbin/bash
+#Author bahauddin
+#tool hacking
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+NOW=`date "+%d.%m.%Y"`
+TIME=`date "+%H:%M"`
+
+echo
+toilet -f standard " InYourDfv47 " -F gay
+echo
+echo ""
+echo -e  "_____________________________________________________________"
+echo -e  "Tools    : 0daysecurity98 $white         " |lolcat
+echo -e  "Creadby  : 0day $white   " |lolcat
+echo -e  "Contact  : attackofcyber969@gmail.com $white " |lolcat
+echo -e  "_____________________________________________________________"
+echo""
+date 
+echo "\033[36;1m"
+echo "[+]===============================================[+]"
+echo $red"|  [1] Materi Hacking Ala InYourDfv47               |"
+echo "[+]===============================================[+]"
+echo $white"|  [2] DDOS Website Dengan LITEDDOS                 |"
+echo "[+]===============================================[+]"
+echo $purple"|  [3] DDOS Website Dengan Torshammer               |"
+echo "[+]===============================================[+]"
+echo $cyan"|  [4] DDOS Website Dengan Xerxes                   |"
+echo "[+]===============================================[+]"
+echo $green"|  [5] DDOS Website Dengan Hammer                   |"
+echo "[+]===============================================[+]"
+echo $yellow"|  [6] Mempercepat / Menstabilkan Sinyal 1          |"
+echo "[+]===============================================[+]"
+echo $red"|  [7] Mempercepat / Menstabilkan Sinyal 2          |"
+echo "[+]===============================================[+]"
+echo $white"|  [8] Install Red_Hawk                             |"
+echo "[+]===============================================[+]"
+echo $purple"|  [9] Instal Txtool                                |"
+echo "[+]===============================================[+]"
+echo $cyan"|  [10] Install Sqlmap Buat Inject Situs            |"
+echo "[+]===============================================[+]"
