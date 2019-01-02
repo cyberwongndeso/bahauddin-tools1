b186f25948926047503e77a74b98d9c77ff17494
cyberwongndeso <42183115+cyberwongndeso@users.noreply.github.com>
2019-01-02 13:54:34 +0700

    Hhgg
    Vvggg

---
+++ toolq.sh
@@ -0,0 +1,205 @@
+#bahauddin/bin/bash
+#Author bahauddin
+#Selamat Belajar
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'    
+
+figlet bahauddin | lolcat
+echo $cyan"TOOL Bahauddin"
+echo $green"Daftar Tools"
+echo $purple"[1] HACK FACEBOOK MBF"
+echo $purple"[2] SQLMAP"
+echo $purple"[3] SHELLPHISH"
+echo $purple"[4] SQLMATE"
+echo $purple"[5] SPAM WHATSAPP"
+echo $purple"[6] LITEDDOS"
+echo $purple"[7] BLACK HYDRA"
+echo $purple"[8] AOC DEFACE"
+echo $purple"[9] SQLI SCAN"
+echo $purple"[10] SQL SCAN"
+echo $purple"[11] RED HAWK"
+echo $purple"[12] CREATER VIRUS"
+echo $purple"[13] ULTIMATE DORK"
+echo
+echo $green
+
+read -p "╭─>[Pilih Nomer]
+╰─>root@bahauddin=•>" ganteng
+ 
+
+if [ $ganteng = 1 ] || [ $ganteng = 1 ]
+then
+clear
+apt update
+apt upgrade 
+apt install python2 
+pip2 install mechanize
+apt install git
+git clone https://github.com/hnov7/mbf/
+cd mbf
+python2 mbf.py
+fi
+
+if [ $ganteng = 2 ] || [ $ganteng = 2 ]
+then
+clear
+pkg update && pkg upgrade 
+pkg install python2
+pkg install git
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py
+fi
+
+if [ $ganteng = 3 ] || [ $ganteng = 3 ]
+then
+clear
+sleep 1
+apt update -y
+apt upgrade -y
+apt install git
+apt install php
+apt install wget
+apt install curl
+apt install openssh
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $ganteng = 4 ] || [ $ganteng = 4 ]
+then
+clear
+sleep 1
+apt update && apt upgrade
+apt install python2
+apt install git
+pip2 install mechanize bs4 HTMLparser argparse requests urlparse2
+git clone https://github.com/UltimateHackers/sqlmate
+cd sqlmate
+chmod +x sqlmate
+python2 sqlmate
+fi
+
+if [ $ganteng = 5 ] || [ $ganteng = 5 ]
+then
+clear
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install php
+git clone https://github.com/siputra12/prank/
+cd prank
+php wa.php
+if
+
+if [ $ganteng = 6 ] || [ $ganteng = 6 ]
+then
+clear
+sleep 1
+apt update && apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 LITEDDOS.py
+fi
+
+if [ $ganteng = 7 ] || [ $ganteng = 7 ]
+then
+clear
+sleep 1
+apt update && apt upgrade
+apt install git hydra
+git clone https://github.com/Gameye98/Black-Hydra/b-hydra/
+cd b-hydra
+mv blackhydra.py main.py
+chmod +x main.py
+python2 main.py
+fi
+
+if [ $ganteng = 8 ] || [ $ganteng = 8 ]
+then
+clear
+sleep 1
+apt update
+apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/Amriez/AOCDEFACE.git
+cd AOCDEDAFACE
+sh install.sh
+sh AOC.sh
+if
+
+if [ $ganteng = 9 ] || [ $ganteng = 9 ]
+then
+clear
+sleep 1
+apt update && apt upgrade
+apt install git
+apt install curl
+git clone https://github.com/thelinuxchoice/sqliscan/
+cd sqliscan
+bash sqliscan.sh
+if
+
+if [ $ganteng = 10 ] || [ $ganteng = 10 ]
+then
+clear
+sleep 1
+apt update
+apt upgrade
+pkg install php
+pkg install git
+git clone https://www.github.com/Cvar1984/sqlscan
+ls
+cd sqlscan
+chmod +x sqlscan.php
+php sqlscan.php
+if
+
+if [ $ganteng = 11 ] || [ $ganteng = 11 ]
+then
+clear
+sleep 1
+apt update && apt upgrade
+pkg install php git
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+if
+
+if [ $ganteng = 12 ] || [ $ganteng = 12 ]
+then
+clear
+sleep 1
+apt update && apt upgrade
+apt install python2
+gem install lolcat
+apt install figlet
+apt install git
+pip2 install mechanize
+git clone https://github.com/virus09/CreaterVirus.git
+cd CreaterVirus
+python2 creater.py
+if 
+
+if [ $ganteng = 13 ] || [ $ganteng = 13 ]
+then
+clear
+sleep 1
+pkg update && pkg upgrade
+pkg install git python2
+git clone https://github.com/jaxBCD/Ultimate-Dork.git
+cd Ultimate-Dork
+pip2 install -r requirements.txt
+python2 ultimate-dork.py
+if
